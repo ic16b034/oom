230d2a86ae5d0e00cbfada7003c2e4b46930ffbf
ic16b034 <friedrich.nemec@technikum-wien.at>
2017-03-07 18:07:28 +0100

    add project Task2

---
+++ tasks/Task2/Task2/Main_program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Task2
+{
+    class Main_program
+    {
+        public static void Main(string[] args)
+		{
+            var studenten = new[]
+        { 
+            new Studenten("Friedrich", "Nemec", "27.11.1981", "XXX"),
+            new Studenten("Anoton", "Huber", "01.10.192", "YYY"),
+            new Studenten("Robert", "Franzen", "20.01.1971", "ZZZ"),
+            new Studenten("Anna", "Berger", "07.03.1990", "VVV"),
+            new Studenten("Leo", "Hauer", "06.04.1995", "WWW")
+        }; 
+ 
+ 
+ 			foreach (var student in studenten) 
+ 			{ 
+ 				Console.WriteLine(  "Name: "+student.Vorname +" "+ student.Nachname +"\n" + 
+                                    "Geburtsdatum: "+ student.Geburtsdatum +"\n" + 
+                                    "Lieblingsfach: "+student.Lieblingsfach+"\n" + 
+                                    "Matrikelnummer: "+ student.Matrikelnummer+"\n" ); 
+ 			} 
+ 
+		} 
+
+       
+    }
+}
---
+++ tasks/Task2/Task2/Studenten.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Globalization; 
+using System.Net; 
+
+ 
+ namespace Task2
+ { 
+ 	public class Studenten
+    { 
+
+        private static int zaehler  = 1000;
+
+        public Studenten(string vorname, string nachname, string geburtsdatum, string lieblingsfach)
+ 		{ 
+             
+			Vorname = vorname;
+            Nachname = nachname;
+            Geburtsdatum = geburtsdatum;
+            Lieblingsfach = lieblingsfach;
+
+            Matrikelnummer = zaehlerup(1);
+
+        } 
+
+        public int zaehlerup(int x)
+ 		{
+            zaehler = zaehler + 1;
+            return zaehler;
+ 		} 
+ 
+
+ 		public string Vorname { get; }
+        public string Nachname { get; }
+        public string Geburtsdatum { get; }
+        public string Lieblingsfach { get; }
+        public int Matrikelnummer { get; }
+
+
+    } 
+} 
+
