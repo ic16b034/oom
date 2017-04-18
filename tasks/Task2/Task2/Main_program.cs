52d474c9be0083214640e7e9b556a75b6eb410d9
ic16b034 <friedrich.nemec@technikum-wien.at>
2017-04-18 19:27:25 +0200

    Task 6

--- tasks/Task2/Task2/Main_program.cs
+++ tasks/Task2/Task2/Main_program.cs
@@ -28,25 +28,21 @@ namespace Task2
             };
 
             //-------------------------------------------------------------------------------------------------------
-            // 1. serialize a single book to a JSON string
+
             Console.WriteLine(JsonConvert.SerializeObject(koerper));
 
-            // 2. ... with nicer formatting
+        
             Console.WriteLine(JsonConvert.SerializeObject(koerper, Formatting.Indented));
 
-            // 3. serialize all items
-            // ... include type, so we can deserialize sub-classes to interface type
             var settings = new JsonSerializerSettings() { Formatting = Formatting.Indented, TypeNameHandling = TypeNameHandling.Auto };
             Console.WriteLine(JsonConvert.SerializeObject(koerper, settings));
 
-            // 4. store json string to file "items.json" on your Desktop
             var text = JsonConvert.SerializeObject(koerper, settings);
             var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             var filename = Path.Combine(desktop, "items.json");
             File.WriteAllText(filename, text);
 
-            // 5. deserialize items from "items.json"
-            // ... and print Description and Price of deserialized items
+  
             var textFromFile = File.ReadAllText(filename);
             var itemsFromFile = JsonConvert.DeserializeObject<Form[]>(textFromFile, settings);
 
